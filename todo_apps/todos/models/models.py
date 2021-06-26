eee9cb441600a1591e584c78443e24000604ddc8
Mazev <77510376+Mazev@users.noreply.github.com>
2021-06-26 23:05:38 +0300

    new

--- todo_apps/todos/models/models.py
+++ todo_apps/todos/models/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Todo(models.Model):
     text = models.CharField(max_length=30)
     state = models.BooleanField(default=False)
-    person_responsible = models.CharField(max_length=50)
+    person_responsible = models.CharField(max_length=50, null=True)
